720c8d7beea52ed1c0f2c88155141b5fba0c8162
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-06-14 13:34:29 -0400

    Test API completed

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 var express = require("express");
 var app = express();
 
+app.use("/api/test", function(req, res) {
+	res.send("Hello World!");
+});
+
+
+
+
+
+
+
 
 
 app.listen(3000, function() {
