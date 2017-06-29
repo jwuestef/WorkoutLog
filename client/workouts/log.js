605858502748c064c1262baf796ef10f616ba86e
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-06-29 09:46:34 -0400

    end of module22-delete

--- client/workouts/log.js
+++ client/workouts/log.js
@@ -21,7 +21,15 @@ $(function() {
 				var len = history.length;
 				var lis = "";
 				for (var i=0; i<len; i++) {
-					lis += "<li class='list-group-item'>" + history[i].def + " - " + history[i].result + "</li>";
+					lis += 
+						"<li class='list-group-item'>" 
+						+ history[i].def 
+						+ " - " 
+						+ history[i].result 
+						+ "<div class='pull-right'>"
+							+ "<button id='" + history[i].id + "' class='update'><strong>U</strong></button>"
+							+ "<button id='" + history[i].id + "' class='remove'><strong>X</strong></button>"
+						+ "</div></li>";
 				}
 				$("#history-list").children().remove();
 				$("#history-list").append(lis);
@@ -44,6 +52,32 @@ $(function() {
 					$("a[href='#history']").tab("show");   //routing
 					$("#log-description").val("");
 					$("#log-logtype").val("");
+				});
+			},
+			delete: function() {
+				var thisLog = {
+					//"this" is the button on the li
+					//.attr("id") targets the value of the id attribute of button
+					id: $(this).attr("id")
+				};
+				var deleteData = {log: thisLog};
+				var deleteLog = $.ajax({
+					type: "DELETE",
+					url: WorkoutLog.API_BASE + "log",
+					data: JSON.stringify(deleteData),
+					contentType: "application/json"
+				});
+				//removes list item
+				//references button then grabs closest li
+				$(this).closest("li").remove();
+				//deletes item out of workouts array
+				for(var i=0; i<WorkoutLog.log.workouts.length; i++) {
+					if(WorkoutLog.log.workouts[i].id == thisLog.id) {
+						WorkoutLog.log.workouts.splice(i, 1);
+					}
+				}
+				deleteLog.fail(function() {
+					console.log("Nope, you didn't delete it");
 				});
 			},
 			fetchAll: function() {
@@ -72,7 +106,9 @@ $(function() {
 	$("#log-save").on("click", WorkoutLog.log.create);
 	if (window.localStorage.getItem("sessionToken")) {
 		WorkoutLog.log.fetchAll();
-	}
+	};
+	$("#history-list").delegate(".remove", "click", WorkoutLog.log.delete);
+
 
 
 
